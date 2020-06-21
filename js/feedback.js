fd3beccdb6a9014fa0452b06a292861cce3567c9
Ediafeer <maksgv@gmail.com>
2020-06-21 15:27:35 +0300

    Сделал Js обратной связи

---
+++ js/feedback.js
@@ -0,0 +1,56 @@
+var feedbackLink = document.querySelector(".feedback-button");
+var feedbackPopup = document.querySelector(".modal-feedback");
+var feedbackClose = feedbackPopup.querySelector(".modal-close");
+var feedbackForm = feedbackPopup.querySelector(".form-feedback");
+var feedbackName = feedbackPopup.querySelector(".your-name-input");
+var feedbackEmail = feedbackPopup.querySelector(".ap-email-input");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+    storage = localStorage.getItem("login");
+} catch (err) {
+    isStorageSupport = false;
+}
+
+feedbackLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    feedbackPopup.classList.add("modal-feedback-snow");
+
+    if (storage) {
+        feedbackName.value = storage;
+        feedbackEmail.focus();
+    } else {
+    feedbackName.focus();
+    }
+});
+
+feedbackClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    feedbackPopup.classList.remove("modal-feedback-snow");
+    feedbackPopup.classList.remove("modal-error");
+});
+
+feedbackForm.addEventListener("submit", function (evt) {
+    if (!feedbackName.value || !feedbackEmail.value) {
+    evt.preventDefault();
+    feedbackPopup.classList.add("modal-error");
+    feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
+    feedbackPopup.classList.add("modal-error");    
+    } else {
+        if (isStorageSupport) {
+        localStorage.setItem("name", feedbackName.value);
+        }
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        if (feedbackPopup.classList.contains("modal-feedback-snow")) {
+        evt.preventDefault();
+        feedbackPopup.classList.remove("modal-feedback-snow");
+        feedbackPopup.classList.remove("modal-error");
+        }
+    }
+});
