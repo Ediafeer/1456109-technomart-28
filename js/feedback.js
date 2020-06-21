92beb92a3fbeee301e4e3c83570ca6eacf30407d
Ediafeer <maksgv@gmail.com>
2020-06-21 16:30:59 +0300

    Начал делать js карты

--- js/feedback.js
+++ js/feedback.js
@@ -16,7 +16,7 @@ try {
 
 feedbackLink.addEventListener("click", function (evt) {
     evt.preventDefault();
-    feedbackPopup.classList.add("modal-feedback-snow");
+    feedbackPopup.classList.add("modal-snow");
 
     if (storage) {
         feedbackName.value = storage;
@@ -28,7 +28,7 @@ feedbackLink.addEventListener("click", function (evt) {
 
 feedbackClose.addEventListener("click", function (evt) {
     evt.preventDefault();
-    feedbackPopup.classList.remove("modal-feedback-snow");
+    feedbackPopup.classList.remove("modal-snow");
     feedbackPopup.classList.remove("modal-error");
 });
 
@@ -47,10 +47,22 @@ feedbackForm.addEventListener("submit", function (evt) {
 
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
-        if (feedbackPopup.classList.contains("modal-feedback-snow")) {
+        if (feedbackPopup.classList.contains("modal-snow")) {
         evt.preventDefault();
-        feedbackPopup.classList.remove("modal-feedback-snow");
+        feedbackPopup.classList.remove("modal-snow");
         feedbackPopup.classList.remove("modal-error");
         }
     }
 });
+
+
+
+var mapLink = document.querySelector(".contacts-button-map");
+var mapPopup = document.querySelector(".modal-map");
+
+
+mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-snow");
+});
+
